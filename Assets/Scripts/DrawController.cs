58e7d5de010828dece59a8dd354d3a63df4ce1c8
RianGoossens <riangoossens@gmail.com>
2015-02-26 17:50:28 +0100

    Improved drawing script, adjusted for isometric view

--- Assets/Scripts/DrawController.cs
+++ Assets/Scripts/DrawController.cs
@@ -14,15 +14,15 @@ public class DrawController : MonoBehaviour {
     void Update() {
         var mouse2D = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
         var mouse3D = Camera.main.ScreenToWorldPoint(mouse2D);
-        var dir = (mouse3D - Camera.main.transform.position).normalized;
-        var origin = Camera.main.transform.position;
+        var dir = Camera.main.transform.forward;
+        var origin = mouse3D;
         Debug.DrawRay(origin, dir*100,Color.white,0.1f);
         if(Input.GetMouseButtonDown(0)){
             Debug.Log("Press "+origin+" "+dir);
             RaycastHit info;
             if (Physics.Raycast(origin, dir, out info)) {
-                var hitPosition = new Vector3((int)info.point.x, ((int)info.point.y) + 0.5f, (int)info.point.z);
-                Debug.Log(hitPosition);
+                var hitPosition = new Vector3(Mathf.Floor(info.point.x+0.5f), 0.5f, Mathf.Floor(info.point.z+0.5f));
+                Debug.Log(hitPosition +" "+ info.point);
                 GameObject.Instantiate(toDraw, hitPosition, toDraw.transform.rotation);
             }
         }
