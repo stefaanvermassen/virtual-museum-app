a9261f135339807dcc28a26056301ab954fa6326
RianGoossens <riangoossens@gmail.com>
2015-02-26 17:23:32 +0100

    Created IsometricCamera script

---
+++ Assets/Scripts/IsometricCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsometricCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var camera = GetComponent<Camera>();
+        camera.orthographic = true;
+        camera.orthographicSize = 8;
+        camera.transform.rotation = Quaternion.Euler(new Vector3(45, 45, 0));
+        camera.transform.transform.position = new Vector3(-10, 14, -10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
