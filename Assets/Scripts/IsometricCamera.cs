e8c1eb96b629a71b551162dd55500a9a5fa17cce
RianGoossens <riangoossens@gmail.com>
2015-02-27 17:17:26 +0100

    Added a sprite and a UI standard style we can all work with

--- Assets/Scripts/IsometricCamera.cs
+++ Assets/Scripts/IsometricCamera.cs
@@ -11,6 +11,10 @@ public class IsometricCamera : MonoBehaviour {
         camera.transform.rotation = Quaternion.Euler(new Vector3(45, 45, 0));
         camera.transform.transform.position = new Vector3(-10, 14, -10);
 	}
+
+    public void Move(float x) {
+        gameObject.transform.position += new Vector3(x,0,0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
