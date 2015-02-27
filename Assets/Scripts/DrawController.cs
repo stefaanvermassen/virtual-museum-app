908b68e89a39c08dffabe60878bb5e46864cf3b4
RianGoossens <riangoossens@gmail.com>
2015-02-27 15:46:28 +0100

    Added camera movement to drawing script

--- Assets/Scripts/DrawController.cs
+++ Assets/Scripts/DrawController.cs
@@ -5,6 +5,8 @@ public class DrawController : MonoBehaviour {
 
     public GameObject toDraw;
     public Museum currentMuseum;
+    public float cameraSpeed = 10;
+    public float edgeRatio = 0.05f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,5 +29,10 @@ public class DrawController : MonoBehaviour {
                 currentMuseum.SetTile((int)Mathf.Floor(info.point.x+0.5f), 0, (int)Mathf.Floor(info.point.z+0.5f),0,0,0);
             }
         }
+        var mouseRatio = new Vector2(mouse2D.x / Display.main.renderingWidth, mouse2D.y / Display.main.renderingHeight);
+        if (mouseRatio.x < edgeRatio)       Camera.main.transform.position += new Vector3(-cameraSpeed, 0, 0) * Time.deltaTime;
+        if (mouseRatio.x > 1 - edgeRatio)   Camera.main.transform.position += new Vector3(cameraSpeed, 0, 0) * Time.deltaTime;
+        if (mouseRatio.y < edgeRatio)       Camera.main.transform.position += new Vector3(0, 0, -cameraSpeed) * Time.deltaTime;
+        if (mouseRatio.y > 1 - edgeRatio)   Camera.main.transform.position += new Vector3(0, 0, cameraSpeed) * Time.deltaTime;
 	}
 }
