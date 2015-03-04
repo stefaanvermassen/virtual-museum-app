ec6f4906ad5f710eeb9067e88f59e07b971e5b99
pieterdc1 <pieter.decremer@ugent.be>
2015-03-04 18:06:19 +0100

    add untracked file

---
+++ Assets/Scripts/ScanEngine/ArtFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scanning
+{
+    public class ArtFilter : Scannable
+    {
+
+        public ArtFilter()
+        {
+
+        }
+
+        public string ArtistName { get; set; }
+        public List<string> Tags { get; set; }
+        public List<string> Genres { get; set; }
+
+
+        string Scannable.GetUniqueString()
+        {
+            String s = "artist:" + ArtistName + ":tags:";
+            foreach (string tag in Tags)
+            {
+                s += tag + ",";
+            }
+            s = s.Substring(0, s.Length - 1);
+            s += ":genres:";
+            foreach (string genre in Genres)
+            {
+                s += genre + ",";
+            }
+            s = s.Substring(0, s.Length - 1);
+            return s;
+        }
+    }
+}
