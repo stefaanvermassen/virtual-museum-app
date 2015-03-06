2402b2823711853fd871b1f493967021e98f9609
pieterdc1 <pieter.decremer@ugent.be>
2015-03-06 15:56:18 +0100

    working on QRCode GUI

--- Assets/Scripts/ScanEngine/QRCode.cs
+++ Assets/Scripts/ScanEngine/QRCode.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Drawing;
+using System.Threading;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
 namespace Scanning
 {
     public class QRCode: ScanIdentity
     {
 
-        public Bitmap Image { get; set; }
+        public Color32[] Image { get; set; }
 
         public QRCode():base(ScanTechnology.QR)
         {
--- Assets/Scripts/ScanEngine/QRScanner.cs
+++ Assets/Scripts/ScanEngine/QRScanner.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-using QRCoder;
 using System;
+using System.Threading;
+using ZXing;
+using ZXing.QrCode;
 
 
 namespace Scanning
@@ -9,27 +11,74 @@ namespace Scanning
     public class QRScanner : Scanner
     {
 
+        //CamTexture size of image to be scanned
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Color32[] Color { get; set; }
+
+        public bool IsQuit { get; set; }
+        public string LastResult { get; set; }
+
+        private bool ShouldEncodeNow { get; set; }
+
         //empty constructor
         public QRScanner()
         {
+            
         }
 
         public ScanIdentity MakeScannable(ScanIdentity scanId, Scannable scannable)
         {
             //for QR codes scanId will be null
-            //TODO: implement
             QRCode id = new QRCode();
 
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(scannable.GetUniqueString(), QRCodeGenerator.ECCLevel.Q);
-            id.Image = qrCode.GetGraphic(20);
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    //size of code writer
+                    Height = Height,
+                    Width = Width
+                }
+            };
+
+            id.Image = writer.Write(scannable.GetUniqueString());
             return id;
         }
 
         public Scannable Scan(ScanIdentity scan)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            ArtFilter filter = new ArtFilter();
+            // create a reader with a custom luminance source
+            var barcodeReader = new BarcodeReader { AutoRotate = false, TryHarder = false };
+
+            while (true)
+            {
+                if (IsQuit)
+                    break;
+
+                try
+                {
+                    // decode the current frame
+                    var result = barcodeReader.Decode(Color, Width, Height);
+                    if (result != null)
+                    {
+                        LastResult = result.Text;
+                        ShouldEncodeNow = true;
+                        filter.configure(result.Text);
+                        return filter;
+                    }
+
+                    // Sleep a little bit and set the signal to get the next frame
+                    Thread.Sleep(200);
+                    Color = null;
+                }
+                catch
+                {
+                }
+            }
+            throw new TimeoutException("Scanning of QR code was interrupted");
         }
     }
 }
