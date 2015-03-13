cb76f9f2706634c872f645af47a3f29d40b3eb4e
pieterdc1 <pieter.decremer@ugent.be>
2015-03-13 19:17:31 +0100

    Refactoring and cleaning some code

--- Assets/Scripts/Data/Storage.cs
+++ Assets/Scripts/Data/Storage.cs
@@ -3,23 +3,29 @@ using System.Collections;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
+/**
+ * This class is implemented using the Singleton design and is used to abstract Saving and loading to local or remote storage
+ * */
 public class Storage : MonoBehaviour {
 
     public enum StoreMode { Only_Local, Only_Remote, Always_Local_Remote_On_Wifi, Local_And_Remote };
 
+    //static variables
     private static Storage STORAGE;
-    private string STORE_MODE_STRING = "StoreMode";
+    private static string STORE_MODE_STRING = "StoreMode";
     public static StoreMode MODE;
 
-    private string MUSEUM_FOLDER ="/museums/";
+    //non-static variables
+    private string MuseumFolder ="/museums/";
 
     public Storage()
     {
+        //empty constructor
     }
 
 	// Use this for initialization
 	void Start () {
-        DontDestroyOnLoad(gameObject); //This object will persist between all scenes
+        DontDestroyOnLoad(gameObject); //This object will persist between all scenes-> the Storage singleton will not be destroyed
 	}
 	
 	// Update is called once per frame
@@ -27,6 +33,9 @@ public class Storage : MonoBehaviour {
 	
 	}
 
+    /**
+     * Mmethod to get the instance of storage to work with
+     * */
     public static Storage get()
     {
         if (STORAGE == null)
@@ -37,6 +46,9 @@ public class Storage : MonoBehaviour {
         return STORAGE;
     }
 
+    /**
+     * Method to Save a Storable object, the player preferences and application runtime platform will decide wether this is done local or remote or both
+     * */
     public void Save<T>(Storable<T, Data<T>> st)
     {
         if (MODE == StoreMode.Only_Local || MODE == StoreMode.Local_And_Remote || MODE == StoreMode.Always_Local_Remote_On_Wifi)
@@ -44,13 +56,12 @@ public class Storage : MonoBehaviour {
             SaveLocal(st);
         }
 
-        //refactor: aparte calls voor leesbaarheid -> Done !
-
+        //DONE: refactor aparte calls voor leesbaarheid
         bool wifi = (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork);
-        bool internet = wifi || ( Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)
+        bool internet = wifi || (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork);
 
-        if(MODE == StoreMode.Always_Local_Remote_On_Wifi && wifi){
-            SaveRemote(st);
+        if(MODE == StoreMode.Always_Local_Remote_On_Wifi){
+            if(wifi) SaveRemote(st);
         }
 
         if(MODE == StoreMode.Only_Remote || MODE == StoreMode.Local_And_Remote)
@@ -68,7 +79,7 @@ public class Storage : MonoBehaviour {
     }
 
 
-    public void SaveLocal<T>(Storable<T,Data<T>> st)
+    private void SaveLocal<T>(Storable<T,Data<T>> st)
     {
         //Require data to save
         var data = st.Save();
@@ -90,21 +101,32 @@ public class Storage : MonoBehaviour {
         if (typeof(T) == typeof(Museum))
         {
             string museumName = ((Museum)st).museumName.Replace(' ', '_');
-            path += MUSEUM_FOLDER + museumName + ".mus";
+            path += MuseumFolder + museumName + ".mus";
         }
 
         return path;
     }
 
+    /**
+     * Method to load a Storable object from storage
+     * */
     public void Load()
+        //TODO: implement
     {
     }
 
+
+    /**
+     * Method to request the Modus of storing
+     * */
     public static StoreMode getStoreMode()
     {
         return MODE;
     }
 
+    /**
+     * 
+     * */
     public static void setStoreMode(StoreMode m)
     {
         MODE = m;
