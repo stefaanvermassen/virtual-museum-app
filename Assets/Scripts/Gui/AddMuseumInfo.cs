97e125903d469c5e761473eb584922e0befd8792
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-14 19:29:09 +0100

    Basic setup

---
+++ Assets/Scripts/Gui/AddMuseumInfo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddMuseumInfo {
+
+    private InputField museumAuthor;
+    private InputField museumName;
+    private InputField museumDescription;
+    private Museum museum;
+
+    public AddMuseumInfo(Museum museum, InputField museumAuthor, InputField museumName, InputField museumDescription)
+    {
+        this.museum = museum;
+        this.museumAuthor = museumAuthor;
+        this.museumName = museumName;
+        this.museumDescription = museumDescription;
+    }
+
+	// Use this for initialization
+    protected void Start()
+    {
+        museumAuthor.text = museum.author;
+        museumName.text = museum.museumName;
+        museumDescription.text = museum.description;
+	}
+
+    public void save()
+    {
+        museum.author = museumAuthor.text;
+        museum.museumName = museumName.text;
+        museum.description = museumDescription.text;
+        Debug.Log("New data written");
+    }
+}
---
+++ Assets/Scripts/Gui/AddMuseumInfoInit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class AddMuseumInfoInit : MonoBehaviour {
+    public InputField museumAuthor;
+    public InputField museumName;
+    public InputField museumDescription;
+
+	// Use this for initialization
+	void Start () {
+        MuseumData museumData = new MuseumData(new List<MuseumTileData>(), new List<MuseumArtData>(), 
+            new List<MuseumObjectData>(), "Ikke", "Fancy", "");
+        Museum museum = gameObject.AddComponent<Museum>();
+        museum.Load(museumData);
+        new AddMuseumInfo(museum, museumAuthor, museumName, museumDescription);
+	}
+}
