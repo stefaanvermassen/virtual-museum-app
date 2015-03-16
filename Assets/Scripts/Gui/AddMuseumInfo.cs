afca779a04987a7e91fcdec92dcbbc5e8dd9ced0
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-16 22:51:13 +0100

    Finished

--- Assets/Scripts/Gui/AddMuseumInfo.cs
+++ Assets/Scripts/Gui/AddMuseumInfo.cs
@@ -1,35 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class AddMuseumInfo {
+public class AddMuseumInfo : MonoBehaviour {
 
-    private InputField museumAuthor;
-    private InputField museumName;
-    private InputField museumDescription;
-    private Museum museum;
+    public InputField museumAuthor;
+    public InputField museumName;
+    public InputField museumDescription;
+    public GameObject panel;
+    public Museum museum;
 
-    public AddMuseumInfo(Museum museum, InputField museumAuthor, InputField museumName, InputField museumDescription)
-    {
-        this.museum = museum;
-        this.museumAuthor = museumAuthor;
-        this.museumName = museumName;
-        this.museumDescription = museumDescription;
-    }
-
-	// Use this for initialization
-    protected void Start()
+    private void Start()
     {
         museumAuthor.text = museum.author;
         museumName.text = museum.museumName;
         museumDescription.text = museum.description;
 	}
 
-    public void save()
+    public void save(int useless)
     {
         museum.author = museumAuthor.text;
         museum.museumName = museumName.text;
         museum.description = museumDescription.text;
-        Debug.Log("New data written");
+
+        panel.SetActive(false);
     }
 }
