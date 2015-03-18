6193846c8a79e1e62dab0e06dd6415eb09273d21
RianGoossens <riangoossens@gmail.com>
2015-03-18 23:30:31 +0100

    Added automatic object UI, reorganized some folders

--- Assets/Scripts/Catalog/Catalog.cs
+++ Assets/Scripts/Catalog/Catalog.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public static class Catalog {
 
-    private static string[] objects = new string[] { "texmonkey", "Vase1" };
+    public static string[] objects = new string[] { "texmonkey", "Vase1" };
 
     public static GameObject GetObject(int objectID) {
-        return Resources.Load<GameObject>(objects[objectID]);
+        return Resources.Load<GameObject>("Objects/"+objects[objectID]);
     }
 
 }
