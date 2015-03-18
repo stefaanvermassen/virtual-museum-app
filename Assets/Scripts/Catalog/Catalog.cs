14e120375ac91926d4ba68348a37b35310f99b26
RianGoossens <riangoossens@gmail.com>
2015-03-18 23:45:13 +0100

    Added bart's statue to the Catalog, together with a ton of debug monkeys

--- Assets/Scripts/Catalog/Catalog.cs
+++ Assets/Scripts/Catalog/Catalog.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public static class Catalog {
 
-    public static string[] objects = new string[] { "texmonkey", "Vase1" };
+    public static string[] objects = new string[] { "texmonkey", "Vase1", "Statue",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey",
+    "texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey","texmonkey"
+    };
 
     public static GameObject GetObject(int objectID) {
         return Resources.Load<GameObject>("Objects/"+objects[objectID]);
