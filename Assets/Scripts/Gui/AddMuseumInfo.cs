9096a33758357d1827700d592b1fd1f3b186ea39
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-18 01:24:41 +0100

    Added comments

--- Assets/Scripts/Gui/AddMuseumInfo.cs
+++ Assets/Scripts/Gui/AddMuseumInfo.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
+/// <summary>
+/// This class controls the UI to add info to a museum
+/// </summary>
 public class AddMuseumInfo : MonoBehaviour {
 
     public InputField museumAuthor;
@@ -11,6 +14,10 @@ public class AddMuseumInfo : MonoBehaviour {
     public GameObject panel;
     public Museum museum;
 
+    /// <summary>
+    /// This method initializes the values from the UI
+    /// with the values available in the Museum
+    /// </summary>
     private void Start()
     {
         museumAuthor.text = museum.author;
@@ -18,6 +25,11 @@ public class AddMuseumInfo : MonoBehaviour {
         museumDescription.text = museum.description;
 	}
 
+    /// <summary>
+    /// This will save the new Museum info when
+    /// the confirm button is pressed
+    /// </summary>
+    /// <param name="useless">Does nothing but is needed to be seen as a ClickListener</param>
     public void save(int useless)
     {
         museum.author = museumAuthor.text;
