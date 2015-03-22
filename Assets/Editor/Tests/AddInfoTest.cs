b5080aeacd26c46945b0ee5b88cdfcdc640e1f78
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-22 22:48:18 +0100

    Split tests and add a test

--- Assets/Editor/Tests/AddInfoTest.cs
+++ Assets/Editor/Tests/AddInfoTest.cs
@@ -16,51 +16,82 @@ public class AddInfoTest
 	}
 
     [Test]
-    public void testStart()
+    public void Initialize_InitializeInputFields_InitializedFields()
     {
         for (int count = 0; count < TEST_CASES; count++)
         {
-            string ownerid = RandomString(0, 5);
             string name = RandomString(0, 20);
             string description = RandomString(0, 500);
-            Museum museum = createMuseum(ownerid, name, description);
-            var ob = new GameObject();
-            var script = ob.AddComponent<AddMuseumInfo>();
-            GameObject ui = (GameObject)GameObject.Instantiate(Resources.Load("AddInfoUI"));
-
-            script.museum = museum;
-            InputField[] fields = ui.GetComponentsInChildren<InputField>();
-            foreach (InputField i in fields)
-            {
-                if (i.CompareTag("TitleField"))
-                {
-                    script.museumName = i;
-                }
-                else if (i.CompareTag("DescriptionField"))
-                {
-                    script.museumDescription = i;
-                }
-            }
-            script.panel = ui.GetComponent<Canvas>().gameObject;
-
-            script.Start();
+            AddMuseumInfo script = initialize(name, description);
 
             Assert.AreEqual(script.museumName.text, name, "TitleField should be " + name + " but it's " + script.museumName.text);
             Assert.AreEqual(script.museumDescription.text, description, "DescriptionField should be " + description + " but it's " + script.museumDescription.text);
-            testSave(script, name, description);
         }
     }
 
-    private void testSave(AddMuseumInfo script, string name, string description)
+    [Test]
+    public void Save_SaveInfo_InfoUnchanged()
     {
-        script.save(RandomInt(0, int.MaxValue));
-        Assert.AreEqual(script.museum.museumName, name, "MuseumName should be " + name + " but it's " + script.museum.museumName);
-        Assert.AreEqual(script.museum.description, description, "MuseumDescription should be " + description + " but it's " + script.museum.description);
+        for (int count = 0; count < TEST_CASES; count++)
+        {
+            string name = RandomString(0, 20);
+            string description = RandomString(0, 500);
+            AddMuseumInfo script = initialize(name, description);
+
+            script.save(RandomInt(0, int.MaxValue));
+            Assert.AreEqual(script.museum.museumName, name, "MuseumName should be " + name + " but it's " + script.museum.museumName);
+            Assert.AreEqual(script.museum.description, description, "MuseumDescription should be " + description + " but it's " + script.museum.description);
+        }
     }
 
-    private Museum createMuseum(string ownerid, string name, string description)
+    [Test]
+    public void Save_SaveInfo_InfoSaved()
     {
-        MuseumData data = new MuseumData(new List<MuseumTileData>(), new List<MuseumArtData>(), new List<MuseumObjectData>(), ownerid, name, description);
+        for (int count = 0; count < TEST_CASES; count++)
+        {
+            AddMuseumInfo script = initialize(RandomString(0, 20), RandomString(0, 500));
+
+            string name = RandomString(0, 20);
+            string description = RandomString(0, 500);
+            script.museumName.text = name;
+            script.museumDescription.text = description;
+            script.save(RandomInt(0, int.MaxValue));
+
+            Assert.AreEqual(script.museum.museumName, name, "MuseumName should be " + name + " but it's " + script.museum.museumName);
+            Assert.AreEqual(script.museum.description, description, "MuseumDescription should be " + description + " but it's " + script.museum.description);
+        }
+    }
+
+    private AddMuseumInfo initialize(string name, string description)
+    {
+        Museum museum = createMuseum(name, description);
+        var ob = new GameObject();
+        AddMuseumInfo script = ob.AddComponent<AddMuseumInfo>();
+        GameObject ui = (GameObject)GameObject.Instantiate(Resources.Load("AddInfoUI"));
+
+        script.museum = museum;
+        InputField[] fields = ui.GetComponentsInChildren<InputField>();
+        foreach (InputField i in fields)
+        {
+            if (i.CompareTag("TitleField"))
+            {
+                script.museumName = i;
+            }
+            else if (i.CompareTag("DescriptionField"))
+            {
+                script.museumDescription = i;
+            }
+        }
+        script.panel = ui.GetComponent<Canvas>().gameObject;
+
+        script.Start();
+
+        return script;
+    }
+
+    private Museum createMuseum(string name, string description)
+    {
+        MuseumData data = new MuseumData(new List<MuseumTileData>(), new List<MuseumArtData>(), new List<MuseumObjectData>(), RandomString(0, 5), name, description);
         var ob = new GameObject();
         var museum = ob.AddComponent<Museum>();
         museum.Load(data);
