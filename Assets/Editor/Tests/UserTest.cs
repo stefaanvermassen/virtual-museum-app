52087c7aa1460473aef3c5118f1193f9b5de4a2b
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-24 11:33:05 +0100

    Set up tests

---
+++ Assets/Editor/Tests/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+[TestFixture]
+public class UserTest : MonoBehaviour {
+
+    [Test]
+    public void LoadUserInformation_LoadingUserData_UserDataLoaded()
+    {
+        UserData data = new UserData(5, "Bert");
+        var ob = new GameObject();
+        var user = ob.AddComponent<User>();
+        user.Load(data);
+        Assert.AreEqual(user.name, data.Name, "Name should be " + data.Name + " but it's " + user.name);
+    }
+}
