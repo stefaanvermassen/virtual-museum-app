e1c05713bbd7acf71d59e36cdeb3254371829dd7
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-25 17:24:29 +0100

    Finished QR view

---
+++ Assets/Scripts/Gui/QRView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QRView : MonoBehaviour
+{
+    public GameObject panel;
+    public Texture2D image;
+    public Image QR;
+    private bool set;
+
+    /// <summary>
+    /// Initialize the QR view
+    /// </summary>
+    private void Start()
+    {
+        set = false;
+        QR.enabled = false;
+    }
+
+    /// <summary>
+    /// Draws the image
+    /// </summary>
+    private void Update()
+    {
+        if (!set && image != null)
+        {
+            set = true;
+            QR.enabled = true;
+            QR.sprite = Sprite.Create(image, new Rect(0, 0, image.width, image.height), Vector2.zero);
+        }
+    }
+
+    // TODO: Implement save
+}
