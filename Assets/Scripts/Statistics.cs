918e92fbc5bc294885e0a70f8882302efb8b0eb8
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-03-28 23:18:34 +0100

    Added comments

--- Assets/Scripts/Statistics.cs
+++ Assets/Scripts/Statistics.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System;
 
+/// <summary>
+/// A singleton to track our statistics using google analytics
+/// </summary>
 public class Statistics
 {
     private static Statistics statistics;
@@ -13,6 +16,9 @@ public class Statistics
     private const string BUTTON_CATEGORY = "BUTTON";
     private const string BUTTON_EVENT = "BUTTON_CLICKED";
 
+    /// <summary>
+    /// Initialises the singleton
+    /// </summary>
     private Statistics()
     { 
         var ob = new GameObject();
@@ -21,6 +27,10 @@ public class Statistics
         lastScreen = "";
     }
 
+    /// <summary>
+    /// Returns an instance of the statistics, lazy loading
+    /// </summary>
+    /// <returns>The Statistics instance</returns>
     public static Statistics getInstance()
     {
         if (statistics == null)
@@ -30,16 +40,26 @@ public class Statistics
         return statistics;
     }
 
+    /// <summary>
+    /// Starts a google analytics session
+    /// </summary>
     private void StartSession()
     {
         googleAnalytics.StartSession();
     }
 
+    /// <summary>
+    /// Stops the current google analytics session
+    /// </summary>
     private void StopSession()
     {
         googleAnalytics.StopSession();
     }
 
+    /// <summary>
+    /// Register a screen to google analytics
+    /// </summary>
+    /// <param name="screenName">The name of the screen</param>
     public void RegisterScreen(string screenName)
     {
         // Is there a screen active
@@ -58,22 +78,40 @@ public class Statistics
             .SetTimingName(buildName(screenName)));
     }
 
+    /// <summary>
+    /// Build names that contains the screenname if set
+    /// </summary>
+    /// <param name="name">The name to build</param>
+    /// <returns>The built name</returns>
     private string buildName(string name)
     {
         return String.IsNullOrEmpty(lastScreen) ? name : lastScreen + "." + name;
     }
 
-    private string buildException(string ex)
+    /// <summary>
+    /// Build the exception name that contains the screenname if set
+    /// </summary>
+    /// <param name="exceptionDescription">The exception description</param>
+    /// <returns>The build exception name</returns>
+    private string buildException(string exceptionDescription)
     {
-        return String.IsNullOrEmpty(lastScreen) ? ex : lastScreen + ": " + ex;
+        return String.IsNullOrEmpty(lastScreen) ? exceptionDescription : lastScreen + ": " + exceptionDescription;
     }
 
+    /// <summary>
+    /// Log an exception to google analytics
+    /// </summary>
+    /// <param name="description">The description of the exception</param>
     public void LogException(string description)
     {
         googleAnalytics.LogException(new ExceptionHitBuilder()
             .SetExceptionDescription(buildException(description)));
     }
 
+    /// <summary>
+    /// Register a button click
+    /// </summary>
+    /// <param name="buttonName">The name of the clicked button</param>
     public void ButtonClicked(string buttonName)
     {
         googleAnalytics.LogEvent(new EventHitBuilder()
@@ -82,6 +120,9 @@ public class Statistics
             .SetEventLabel(buildName(buttonName)));
     }
 
+    /// <summary>
+    /// Unregister the screen and stop the session
+    /// </summary>
     public void RemoveScreen()
     {
         lastScreen = "";
