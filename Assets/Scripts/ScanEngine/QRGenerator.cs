da60681809fc2bda24a92d99d2f128c5c6dd1105
pieterdc1 <pieter.decremer@ugent.be>
2015-04-04 16:23:51 +0200

    hij verzet die sprite ni en ik snap ni waarom

--- Assets/Scripts/ScanEngine/QRGenerator.cs
+++ Assets/Scripts/ScanEngine/QRGenerator.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using Scanning;
 
 public class QRGenerator : MonoBehaviour {
 
     public ArtFilter RequestedArtFilter { get; set; }
-    public Color32[] QRCodeImage;
+    public Image GUIImage;
 
     public QRGenerator(ArtFilter filter)
     {
@@ -26,15 +27,19 @@ public class QRGenerator : MonoBehaviour {
         Debug.Log("ArtFilter made");
         QRScanner scanner = new QRScanner();
         QRCode qrCode = (QRCode)scanner.MakeScannable(null, RequestedArtFilter);
-        QRCodeImage = qrCode.Image;
+        Color32[] QRCodeImage = qrCode.Image;
         Debug.Log("QRCode image required");
 
-        Texture2D qrTexture = new Texture2D(256, 256);
-        Debug.Log("number of pixels in array:" + QRCodeImage.Length);
-        qrTexture.SetPixels32(0,0,256,256,QRCodeImage);
 
-        GUI.DrawTexture(new Rect(0, 0, 256, 256), qrTexture, ScaleMode.ScaleToFit, true, 10.0f);
-        Debug.Log("Texture drawn");
+        /*Texture2D qrTexture = new Texture2D(256, 256);
+        Debug.Log("number of pixels in array:" + QRCodeImage.Length);
+        qrTexture.SetPixels32(0,0,256,256,QRCodeImage);*/
+        Texture2D qrTexture = new Texture2D(265, 265);
+        qrTexture.SetPixel(0, 0, Color.red);
+
+        Debug.Log("Got image:" + GUIImage.minHeight);
+        GUIImage.color = Color.blue;
+        GUIImage.sprite = Sprite.Create(qrTexture, new Rect(0, 0, 256, 256), Vector2.zero);
     }
 
 	// Update is called once per frame
