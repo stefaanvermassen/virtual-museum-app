680adb6afb6ccda61c4119803cfd9d06cf20c4f3
Bart Middag <bart.middag@gmail.com>
2015-04-22 19:59:56 +0200

    Opening app from links on Android

--- Assets/Scripts/Gui/BasicActions.cs
+++ Assets/Scripts/Gui/BasicActions.cs
@@ -1,10 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.UI;
 
 public class BasicActions : MonoBehaviour {
+
+	void Start () {
+		HandleURI ();
+	}
 
 	public void startGame()
 	{
 		Application.LoadLevel("BuildMuseum");
+	}
+
+	void HandleURI() {
+#if UNITY_ANDROID
+		var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		var currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		var datastring = currentActivity.Call<string>("getIntentDataString");
+		if(datastring.Contains ("museum/")) {
+			string[] splitString = datastring.Split(new string[] {"museum/"}, StringSplitOptions.RemoveEmptyEntries);
+			int id = int.Parse(splitString[splitString.Length-1]);
+			LoadMuseum (id);
+		} else {
+			MuseumLoader.museumID = -1; // Don't load a museum
+		}
+#endif
+	}
+
+	public void LoadMuseum(int id) {
+		MuseumLoader.museumID = id;
+		Application.LoadLevel ("WalkingController");
 	}
 }
--- Assets/Scripts/MuseumLoader.cs
+++ Assets/Scripts/MuseumLoader.cs
@@ -6,16 +6,21 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public class MuseumLoader : MonoBehaviour {
 
+	public static int museumID = -1;
 	public Museum museum;
 
 	// Use this for initialization
 	void Start () {
-		if (File.Exists(Application.persistentDataPath + "/test.bin")) {
-			Stream TestFileStream = File.OpenRead(Application.persistentDataPath + "/test.bin");
-			BinaryFormatter deserializer = new BinaryFormatter();
-			MuseumData data = (MuseumData)deserializer.Deserialize(TestFileStream);
-			TestFileStream.Close();
-			museum.Load(data);
+		if(museumID == -1) { // Preview mode
+			if (File.Exists(Application.persistentDataPath + "/test.bin")) {
+				Stream TestFileStream = File.OpenRead(Application.persistentDataPath + "/test.bin");
+				BinaryFormatter deserializer = new BinaryFormatter();
+				MuseumData data = (MuseumData)deserializer.Deserialize(TestFileStream);
+				TestFileStream.Close();
+				museum.Load(data);
+			}
+		} else { // Visit mode
+			Storage.Instance.LoadRemote(museum, museumID.ToString());
 		}
 	}
 	
