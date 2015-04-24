c157d6b34f150a2a5db5499783023b1d30894286
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-04-24 18:22:34 +0200

    Login done without feedback

---
+++ Assets/Scripts/Gui/LoginView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using HTTP;
+
+/// <summary>
+/// This class controls the UI to login
+/// </summary>
+public class LoginView : MonoBehaviour
+{
+
+    public InputField emailField;
+    public InputField passwordField;
+    public GameObject panel;
+
+    /// <summary>
+    /// This logs in the user
+    /// </summary>
+    /// <param name="useless">Does nothing but is needed to be seen as a ClickListener</param>
+    public void Login(int useless)
+    {
+        string email = emailField.text;
+        string password = passwordField.text;
+
+        API.UserController userController = API.UserController.Instance;
+        Request response = userController.Login(email, password);
+
+        panel.SetActive(false);
+    }
+}
