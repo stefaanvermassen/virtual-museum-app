9e693cb72d3e4fcc70377954f338b0863dc8b35a
Titouan Vervack <Titouan.Vervack@UGent.be>
2015-04-25 01:07:01 +0200

    Added register and some feedback

--- Assets/Scripts/Gui/RegisterView.cs
+++ Assets/Scripts/Gui/RegisterView.cs
@@ -9,32 +9,43 @@ using API;
 /// <summary>
 /// This class controls the UI to login
 /// </summary>
-public class LoginView : MonoBehaviour
+public class RegisterView : MonoBehaviour
 {
 
     public InputField usernameField;
+    public InputField emailField;
     public InputField passwordField;
+    public InputField confirmPasswordField;
     public GameObject panel;
 
     /// <summary>
     /// This logs in the user
     /// </summary>
     /// <param name="useless">Does nothing but is needed to be seen as a ClickListener</param>
-    public void Login(int useless)
+    public void Register(int useless)
     {
         string username = usernameField.text;
+        string email = emailField.text;
         string password = passwordField.text;
-        Toast toast = new Toast();
+        Toast toast = gameObject.AddComponent<Toast>();
 
         if (String.IsNullOrEmpty(password)
-            || String.IsNullOrEmpty(username))
+            || String.IsNullOrEmpty(username)
+            || String.IsNullOrEmpty(email))
         {
             toast.Notify("Please fill in all fields");
             return;
         }
 
+        if (password != confirmPasswordField.text) {
+            toast.Notify("Passwords differ");
+            passwordField.text = "";
+            confirmPasswordField.text = "";
+            return;
+        }
+
         API.UserController userController = API.UserController.Instance;
-        Request response = userController.Login(username, password, (success) =>
+        Request response = userController.CreateUser(username, email, password, (success) =>
         {
             SessionManager.Instance.LoginUser(success);
             toast.Notify("Successfully registered!");
