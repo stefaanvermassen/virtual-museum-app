4fb1126964a273247da7c5433027ffc189c42bab
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2015-04-29 18:05:59 +0200

    Added ArtworkFilterController

---
+++ Assets/Scripts/API/ArtworkFilterController.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HTTP;
+using UnityEngine;
+
+namespace API
+{
+    /// <summary>
+    ///     Artworkfilter controller.
+    ///     updateMuseum
+    /// </summary>
+    public class ArtworkFilterController : APIConnection
+    {
+        private const string ARTWORKFILTER = "artworkfilter";
+
+        protected ArtworkFilterController()
+        {
+        }
+
+        private static readonly ArtworkFilterController _Instance = new ArtworkFilterController();
+
+        /// <summary>
+        ///     Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static ArtworkFilterController Instance
+        {
+            get { return _Instance; }
+        }
+
+    
+
+        /// <summary>
+        ///     Gets all artworkfilters that are connected to the user.
+        /// </summary>
+        /// <returns>The all artworks.</returns>
+        /// <param name="success">Success.</param>
+        /// <param name="error">Error.</param>
+        public Request GetConnectedArtworkFilters(Action<ArrayList> success = null, Action<API_Error> error = null)
+        {
+			return Get(BASE_URL + ARTWORKFILTER + "/connected", (response =>
+			                                         {
+				if (success != null)
+				{
+					var apiList = (ArrayList) response.Object["ArtWorkFilters"];
+					var list = new ArrayList();
+					foreach (Hashtable val in apiList)
+					{
+						list.Add(ArtWorkFilter.Create(val));
+					}
+					success(list);
+				}
+			}), error);
+        }
+
+        /// <summary>
+        /// Creates the art work filter.
+        /// </summary>
+        /// <returns>The art work filter.</returns>
+		/// <param name="filter">ArtWorkFilter is an API class which contains all necessary fields, ArtWorkFilterID can be left empty when
+		///     creating a new museum</param>
+		/// <param name="success">Success. A closure which will be executed when creating the artworkfilter is a succes, gives you a
+		///     artworkfilter object with the needed id.</param>
+		/// <param name="error">Error. Handle errors</param>
+		public Request CreateArtWorkFilter(ArtWorkFilter filter, Action<ArtWorkFilter> success = null, Action<API_Error> error = null)
+		{
+			var form = filter.ToHash();
+			
+			return Post(BASE_URL + ARTWORKFILTER, form, (response =>
+			                                      {
+				if (success != null)
+				{
+					var m = ArtWorkFilter.Create(response.Object);
+					success(m);
+				}
+			}), error, true);
+		}
+    }
+
+    /// <summary>
+    ///     Artworkfilter.
+    /// </summary>
+    public class ArtWorkFilter
+    {
+        public int ArtWorkFilterID;
+ 
+        public string Name = "";
+		public string Value = "";
+    
+
+        public Hashtable ToHash()
+        {
+            var dict = new Hashtable
+            {
+				{"Id", ArtWorkFilterID.ToString()},
+                {"Name", Name},
+                {"Value", Value}
+            };
+
+            return dict;
+        }
+
+        public static ArtWorkFilter Create(Hashtable dict)
+        {
+            var awf = new ArtWorkFilter
+            {
+				ArtWorkFilterID = ((int) dict["Id"]),
+                Name = (string) dict["Name"],
+				Value = (string) dict["Filter"]
+            };
+
+            return awf;
+        }
+
+	
+    }
+
+}
