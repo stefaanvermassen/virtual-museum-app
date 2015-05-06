f8f1a685a6036d3651fe8a054ff09f50b8964531
pieterdc1 <pieter.decremer@ugent.be>
2015-05-06 20:14:35 +0200

    added artId to ArtFilter

--- Assets/Scripts/ScanEngine/ArtFilter.cs
+++ Assets/Scripts/ScanEngine/ArtFilter.cs
@@ -15,7 +15,8 @@ namespace Scanning
             Genres = new List<string>();
         }
 
-        public string ArtistName { get; set; }
+		public string ArtistName { get; set; }
+		public int ArtId { get; set; }
         public List<string> Tags { get; set; }
         public List<string> Genres { get; set; }
 
@@ -23,10 +24,11 @@ namespace Scanning
         public void Configure(string s)
         {
             string[] labels = s.Split('?');
-            string[] tags = labels[3].Split(',');
-            string[] genres = labels[5].Split(',');
+            string[] tags = labels[5].Split(',');
+            string[] genres = labels[7].Split(',');
 
             ArtistName = labels[1];
+			ArtId = labels [3];
             Tags = new List<string>(tags);
             Genres = new List<string>(genres);
         }
@@ -34,7 +36,13 @@ namespace Scanning
         string Scannable.GetUniqueString()
         {
 
-			String s = "museum.awesomepeople.tv/filter/artist?" + ArtistName + "?tags?";
+			String s = "museum.awesomepeople.tv/filter/artist?" + ArtistName;
+			s += "?art?";
+			if (ArtId != null)
+				s += ArtId;
+			else
+				s += "-1";
+			s+="?tags?";
             foreach (string tag in Tags)
             {
                 s += tag + ",";
